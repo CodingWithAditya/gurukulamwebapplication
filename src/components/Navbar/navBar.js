204682eb9862786db31d600fc456d240bd3c04ea
Aditya Tiwari <93578414+CodingWithAditya@users.noreply.github.com>
2022-11-26 10:53:10 +0530

    branch_main

--- src/components/Navbar/navBar.js
+++ src/components/Navbar/navBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <header class="h-24 sticky top-0 z-50">
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 pb-4 h-full dark:bg-red-900">
+    <nav class="bg-red-900 border-gray-200 px-4 lg:px-6 pb-4 h-full dark:bg-red-900">
         <div class="flex flex-wrap items-center ml-14  max-w-screen-2xl">
            
             <img src={require('../../images/headerlogo.png')} alt="log3-copy" border="0" class="mb-3 sm:h-16" />
