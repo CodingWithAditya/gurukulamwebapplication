5dec8692709a5f125e20774c9e7392c8ba5b8bd3
Aditya Tiwari <93578414+CodingWithAditya@users.noreply.github.com>
2022-12-25 16:18:01 +0530

    branch_main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { hydrate, render } from "react-dom";
+ 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +13,12 @@ root.render(
   </React.StrictMode>
 );
 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
